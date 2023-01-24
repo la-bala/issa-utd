76599f78de2f31bc41259f00942a93fd24cdbeb5
th3bala <leoalpbala@gmail.com>
2023-01-23 23:35:34 -0600

    added Jenny Menna talk

--- src/components/NextEvent.vue
+++ src/components/NextEvent.vue
@@ -19,6 +19,34 @@ export default {
         <div class="row justify-content-center">
             <div class="py-4 col-md-8 col-xl-6">
                 <div class="card bg-dark text-white border border-secondary">
+                    <img class="card-img-top" src="../assets/cybertrends.jpeg" alt="Cybersecurity stock photo">
+                    <ul class="list-group list-group-flush border border-0">
+                            <li class="list-group-item border border-0 bg-dark text-white">
+                                <ClockIcon :size="16"/>
+                                <span class="ms-1"> 3-4PM, January 25, 2023</span>
+                            </li>
+                            <li class="list-group-item border border-0 bg-dark text-white" id="meetingLocation">
+                                <LocationIcon :size="16"/>
+                                <span class="ms-1"> GR 2.326 (San Antonio Room)</span>
+                            </li>
+                        </ul>
+                    <div class="d-flex flex-column card-body">
+                        <h5 class="card-title">Cybersecurity Guest Talk: Jenny Menna</h5>
+                        <p class="card-text">
+                            Before the Spring kickoff, join us for a guest talk with Jenny Menna, VP of Threat Management & Response at Humana, board member of the FS-ISAC, and a former senior government executive. Joined by cloud security expert James Binford, she will give a talk on cybersecurity threats, trends, and careers across industry and government!
+                        </p>
+                        <div class="row justify-content-center">
+                            <div class="col-6">
+                                <a class="btn btn-primary" href="https://forms.gle/ia2mQmjxkFjtSsSB9" target="_blank" rel="noopener noreferrer" role="button">I'm Interested</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div hidden class="row justify-content-center">
+            <div class="py-4 col-md-8 col-xl-6">
+                <div class="card bg-dark text-white border border-secondary">
                     <img class="card-img-top" src="../assets/launch.jpg" alt="Meeting image placeholder">
                     <ul class="list-group list-group-flush border border-0">
                             <li class="list-group-item border border-0 bg-dark text-white">
--- src/components/UpcomingEvents.vue
+++ src/components/UpcomingEvents.vue
@@ -7,12 +7,27 @@
             <div class="card text-white ps-2">
                 <div class="row g-0" style="height:100%">
                     <div class="col-4 my-auto">
+                        <img src="../assets/cybertrends.jpeg" class="img-fluid rounded" alt="Cybersecurity stock photo">
+                    </div>
+                    <div class="col-8">
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">Cybersecurity Guest Talk: Jenny Menna</h5>
+                            <p class="card-text">Before the kickoff, join us for a guest talk with Jenny Menna, VP of Threat Management & Response at Humana, board member of the FS-ISAC, and a former senior government executive. Joined by cloud security expert James Binford, she will give a talk on cybersecurity threats, trends, and careers across industry and government! Jan 25, 2023, 3-4PM in GR 2.326 (San Antonio Room).</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card text-white ps-2">
+                <div class="row g-0" style="height:100%">
+                    <div class="col-4 my-auto">
                         <img src="../assets/launch.jpg" class="img-fluid rounded" alt="ISSA kickoff picture">
                     </div>
                     <div class="col-8">
                         <div class="card-body">
                             <h5 class="card-title fw-bold">ISSA UTD Kickoff</h5>
-                            <p class="card-text">Join NTX ISSA president Sean Richards, subchapter director Alena Funtikova-White, and other UTD students interested in cybersecurity for our Spring kickoff! Free food will be provided!</p>
+                            <p class="card-text">Join NTX ISSA president Sean Richards, subchapter director Alena Funtikova-White, and other UTD students interested in cybersecurity for our Spring kickoff! Free food will be provided! Jan 31, 2023, 7PM in SSA 12.471.</p>
                         </div>
                     </div>
                 </div>
